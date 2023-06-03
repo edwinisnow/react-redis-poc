fd5035f623004963418c298691f3722fecc17e9e
edwinisnow <edwin04@gmail.com>
2023-06-03 05:51:47 +0100

    reactstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Home from './components/Home'
 import './styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // To run the build, goto build folder and run npx serve
 export default function App() {
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+  Badge,
+  Card,
+  Container,
+} from 'reactstrap'
 
 function Home() {
   const [threads, setThreads] = useState([])
+  const [open, setOpen] = useState('1')
+  const toggle = (id) => {
+    if (open === id) {
+      setOpen()
+    } else {
+      setOpen(id)
+    }
+  }
   useEffect(() => {
     axios
       .get('http://localhost:3000/threads')
@@ -11,31 +28,59 @@ function Home() {
       })
       .catch((err) => console.log(err))
   }, [])
-  console.log('threads', threads)
+
   return (
     <>
-      <div>Home</div>
-      <p>Total threads: {threads.length}</p>
-      {threads &&
-        threads.map((thread) => (
-          <div key={thread.id}>
-            {thread.commands.map((command) => {
-              return (
-                <div key={command.id}>
-                  {`No. of commands validated: ${command.validated}`}
-                </div>
-              )
-            })}
-          </div>
-        ))}
+      <Container fluid>
+        <Card
+          style={{
+            width: '600px',
+          }}
+        >
+          <h4>
+            <center>
+              Total threads: <Badge>{threads.length}</Badge>
+            </center>
+          </h4>
+          <Accordion open={open} toggle={toggle}>
+            {threads &&
+              threads.map((thread, i) => {
+                const no_validated = thread.commands.reduce(
+                  (acc, curr) => (acc = curr.validated ? acc + 1 : acc),
+                  0
+                )
+                return (
+                  <AccordionItem key={thread.id}>
+                    <AccordionHeader targetId={thread.id}>
+                      Thread {i + 1} : {no_validated}/{thread.commands.length}{' '}
+                      validated
+                    </AccordionHeader>
+                    <AccordionBody accordionId={thread.id}>
+                      {thread.commands.map((command) => {
+                        const color = command.validated ? 'success' : 'danger'
+                        const validateText = command.validated ? 'done' : 'open'
+                        return (
+                          <div key={command.id}>
+                            <p>
+                              {`${command.name}`}
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              <Badge color={color} pill>
+                                {validateText.toUpperCase()}
+                              </Badge>
+                            </p>
+                          </div>
+                        )
+                      })}
+                    </AccordionBody>
+                  </AccordionItem>
+                )
+              })}
+          </Accordion>
+        </Card>
+      </Container>
     </>
   )
 }
 
 export default Home
-
-// Total Threads
-// No. of Runnings Threads (Label)
-
-// Thread->
-// # of commands validated / Total # of commands
